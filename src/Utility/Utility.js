84ee8c49baf1aeffc52480d57215aca25941fc0c
bidduthosen <mdbidduthosen34@gmail.com>
2025-03-03 12:47:19 +0600

    set local storege wishlist book and read book

---
+++ src/Utility/Utility.js
@@ -0,0 +1,46 @@
+
+const getStoredReadCart = () => {
+    const getStoredcartStr = localStorage.getItem('read-item');
+    if (getStoredcartStr) {
+        const storedCart = JSON.parse(getStoredcartStr);
+        return storedCart;
+    } else {
+        return [];
+    }
+};
+
+const addToStoredRead = (id) => {
+    const storedRead = getStoredReadCart();
+    if (storedRead.includes(id)) {
+        console.log(id, 'already added')
+    }
+    else {
+        storedRead.push(id);
+        const storedCartStr =  JSON.stringify(storedRead)
+        localStorage.setItem('read-item', storedCartStr)
+    }
+}
+
+
+
+// wishlist 
+const getStoredWishList  = ()=>{
+    const storedWishListStr  = localStorage.getItem('wishList-item');
+    if(storedWishListStr){
+        return JSON.parse(storedWishListStr)
+    }else{
+        return [];
+    }
+
+}
+const addToStoredWishList = (id) =>{
+    const storedWishList = getStoredWishList();
+    if(storedWishList.includes(id)){
+        console.log(id, 'Already Add')
+    }else{
+        storedWishList.push(id)
+        const storedWishListStr = JSON.stringify(storedWishList)
+        localStorage.setItem( 'wishList-item',storedWishListStr)
+    }
+}
+export { addToStoredRead, addToStoredWishList }
--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
+import { addToStoredRead, addToStoredWishList } from '../../Utility/Utility';
 
 const BookDetails = () => {
+    const navigete = useNavigate()
     const { bookId } = useParams()
     const id = parseInt(bookId)
     const datas = useLoaderData();
     const book = datas.find(data => data.bookId === id);
+
+    const handleReadCart = (id) =>{
+        addToStoredRead(id)
+    }
+    const handleWishListCart = (id) =>{
+        addToStoredWishList(id)
+    }
+
+
     const {bookId: currentBookId, image, bookName, author, review, rating, tags } = book;
     return (
         <div className='bg-gray-300 w-10/12 mx-auto p-7 rounded-xl'>
@@ -17,9 +28,10 @@ const BookDetails = () => {
                 <h1 className='font-bold text-2xl '>{bookName}</h1>
                 <p className='text-gray-500'>{review}</p>
                 <div className='space-x-4 flex justify-end'>
-                    <button className='btn btn-outline btn-secondary'>Read</button>
-                    <button className='btn btn-outline btn-secondary'>wishlist</button>
+                    <button onClick={() => handleReadCart(bookId)} className='btn btn-outline btn-secondary'>Read</button>
+                    <button onClick={()=> handleWishListCart(bookId)} className='btn btn-outline btn-secondary'>wishlist</button>
                 </div>
+                <button onClick={()=> navigete(-1)} className='btn'>Back {`>`}</button>
             </div>
         </div>
     );
